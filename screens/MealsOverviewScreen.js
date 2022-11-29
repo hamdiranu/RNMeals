df9d13e9a768d7d0a2800f855083d063570771df
hamdiranu <hamdira@sci.ui.ac.id>
2022-11-29 16:28:10 +0700

    101 done

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,43 @@
+import { StyleSheet, View, FlatList } from "react-native";
+import { MEALS } from "../data/dummy-data";
+
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealitem(itemData) {
+    const item = itemData.item;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+    return <MealItem {...mealItemProps} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealitem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
